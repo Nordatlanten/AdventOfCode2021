655ff22560d2ded1305f25477cee29b31a60f779
gevaliatlaniskt@gmail.com <atlan@understandit.se>
2021-12-02 00:04:30 +0100

    Got rid of all those parseInt()s

--- day1/part1.js
+++ day1/part1.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const puzzleInput = fs.readFileSync("./input.txt").toString();
-let entries = puzzleInput.split("\n");
+let entries = puzzleInput.split("\n").map((n) => +n);
 let counter = 0;
 for (let i = 0; i < entries.length; i++) {
-  if (parseInt(entries[i]) < parseInt(entries[i + 1])) counter++;
+  if (entries[i] < entries[i + 1]) counter++;
 }
 console.log("The answer is: " + counter);
--- day1/part2.js
+++ day1/part2.js
@@ -1,15 +1,10 @@
 const fs = require("fs");
 const puzzleInput = fs.readFileSync("./input.txt").toString();
-let entries = puzzleInput.split("\n");
+let entries = puzzleInput.split("\n").map((n) => +n);
 let counter = 0;
-
 for (let i = 0; i < entries.length; i++) {
-  let sum1 =
-    parseInt(entries[i]) + parseInt(entries[i + 1]) + parseInt(entries[i + 2]);
-  let sum2 =
-    parseInt(entries[i + 1]) +
-    parseInt(entries[i + 2]) +
-    parseInt(entries[i + 3]);
+  let sum1 = entries[i] + entries[i + 1] + entries[i + 2];
+  let sum2 = entries[i + 1] + entries[i + 2] + entries[i + 3];
   if (sum1 < sum2) counter++;
 }
 console.log("The answer is: " + counter);
